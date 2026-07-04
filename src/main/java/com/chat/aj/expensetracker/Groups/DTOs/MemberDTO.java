a33fc0d839d1da18a5592bf8e5e215b2e2e20798
ArjavTripathi <arjavtri@iastate.edu>
2026-07-04 15:45:45 -0500

    Quality of life changes

---
+++ src/main/java/com/chat/aj/expensetracker/Groups/DTOs/MemberDTO.java
@@ -0,0 +1,13 @@
+package com.chat.aj.expensetracker.Groups.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDTO {
+    private Long id;
+    private String name;
+}
