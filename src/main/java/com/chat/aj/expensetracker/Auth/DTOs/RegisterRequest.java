c327e5a3a9b2402c47babcfd145a1855a10c124d
ArjavTripathi <arjavtri@iastate.edu>
2026-07-16 17:57:58 -0500

    adding auditing

--- src/main/java/com/chat/aj/expensetracker/Auth/DTOs/RegisterRequest.java
+++ src/main/java/com/chat/aj/expensetracker/Auth/DTOs/RegisterRequest.java
@@ -1,5 +1,8 @@
 package com.chat.aj.expensetracker.Auth.DTOs;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     public String email;
+
+    @NotBlank(message = "Name is required")
     public String name;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     public String password;
 }
---
+++ src/main/java/com/chat/aj/expensetracker/common/Entities/Audit.java
@@ -0,0 +1,33 @@
+package com.chat.aj.expensetracker.common.Entities;
+
+
+import com.chat.aj.expensetracker.common.DTOs.RequestType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Audit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String requestId;
+
+    private RequestType requestType;
+
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User requestedBy;
+U
+}
